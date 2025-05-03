463d50d25fc5230bc80e658a26f5223ce1cf6ddd
karthi9772 <karthi.g9772@gmail.com>
2025-05-03 17:29:14 +0530

    final deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API_BASE_URL = 'https://job-management-admin-interface-olive.vercel.app/api';
+const API_BASE_URL = 'https://job-management-admin-interface-olive.vercel.app/api';
 
-const API_BASE_URL = 'http://localhost:5000/api'; 
+// const API_BASE_URL = 'http://localhost:5000/api'; 
 
 // Fetch jobs with optional filters
 export const getJobs = async (filters = {}) => {
