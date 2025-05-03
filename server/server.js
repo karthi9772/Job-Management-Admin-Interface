af929776405541e0d7d6fcf1e99e7f4049f07752
karthi9772 <karthi.g9772@gmail.com>
2025-05-03 16:09:28 +0530

    deploy

---
+++ server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const jobsRoute = require('./routes/jobs');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: [
+      'http://localhost:5173'
+  ],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.use(express.json());
+
+// Routes
+app.use('/api/jobs', jobsRoute);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
